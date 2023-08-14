98d95a690c264537eb8c5daa821a6103a94d0c5b
dd <mail@dandalzotto.com>
2023-08-14 14:18:43 -0400

    yep

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 gsap.to("h1", {opacity: 1, duration: 1, delay: .5});
 
-gsap.to(".rhs h2", {
-  delay: 1,
+gsap.to(".rhs p", {
+  delay: .5,
   duration: 1.5,
   opacity: 1,
-  stagger: .25 // 0.1 seconds between when each ".box" element starts animating
+  stagger: .5 // 0.1 seconds between when each ".box" element starts animating
 });
